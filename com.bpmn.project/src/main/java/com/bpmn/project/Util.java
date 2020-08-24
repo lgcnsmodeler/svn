6754e30868c8c44f339fc1b5a087ff016f197896
MongE <MongE@DESKTOP-9GRVNP4>
2020-08-24 17:27:12 +0900

    YEO : APPLY UTIL.JAVA CLASS

---
+++ com.bpmn.project/src/main/java/com/bpmn/project/Util.java
@@ -0,0 +1,30 @@
+package com.bpmn.project;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
+public class Util {
+	
+    public static Document stringToDom(String xmlSource) 
+            throws SAXException, ParserConfigurationException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(new InputSource(new StringReader(xmlSource)));
+    }
+}
+
+
+
